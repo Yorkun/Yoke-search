50b00fade53854247114d1eee7ec5a525d395e6d
Yorkun <726667671@qq.com>
2019-11-20 15:32:54 +0800

    update css

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-var baidu = document.querySelector('.button_01');
-var inputValue = document.querySelector('input').value
+var baidu = document.querySelector('.button_baidu');
 
 baidu.addEventListener('click', function () {
+    var inputValue = document.querySelector('input').value
     var addHref = 'https://www.baidu.com/s?wd=' + inputValue
     baidu.href = addHref
 })
-
-
-console.log(baidu.href)
