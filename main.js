1818bcb5cea8ec181b0d4977df0e01364f9bcecd
Yorkun <726667671@qq.com>
2019-11-21 13:08:33 +0800

    add google analytics

--- main.js
+++ main.js
@@ -1,50 +1,108 @@
-var baidu = document.querySelector('.button_baidu');
-baidu.addEventListener('click', function () {
+const baidu = document.querySelector('.button_baidu');
+const google = document.querySelector('.button_google');
+const bing = document.querySelector('.button_bing');
+const allweb = document.querySelector('.button_allweb');
+const dribbble = document.querySelector('.button_dribbble');
+const behance = document.querySelector('.button_behance');
+const pinterest = document.querySelector('.button_pinterest');
+const allpic = document.querySelector('.button_allpic');
+const iconfont = document.querySelector('.button_iconfont');
+const unsplash = document.querySelector('.button_unsplash');
+const image = document.querySelector('.button_image');
+
+const sreachArr = [
+    {
+        ele: baidu,
+        url: 'https://www.baidu.com/s?wd='
+    },
+    {
+        ele: google,
+        url: 'https://www.google.com/search?sxsrf=ACYBGNRZUE-YO33VVcWqh2Z1CnoBtiY_UA%3A1574244452133&ei=ZBDVXZffB4K5wAPD9aOIAQ&q='
+    },
+    {
+        ele: bing,
+        url: 'https://www.bing.com/search?q='
+    }
+]
+
+
+sreachArr.forEach(d => {
+    d.ele.addEventListener('click', function () {
+        var inputValue = document.querySelector('input').value;
+        d.ele.href = d.url + inputValue;
+    })
+});
+
+allweb.addEventListener('click',function (){
     var inputValue = document.querySelector('input').value;
-    var addHref = 'https://www.baidu.com/s?wd=' + inputValue;
-    baidu.href = addHref;
+    sreachArr.forEach(d => {
+        window.open(d.url + inputValue)
+    })
+}) 
+
+const sreachArr2 = [
+    {
+        ele: dribbble,
+        url: 'https://dribbble.com/search/'
+    },
+    {
+        ele: behance,
+        url: 'https://www.behance.net/search?search='
+    },
+    {
+        ele: pinterest,
+        url: 'https://www.pinterest.com/search/pins/?q='
+    }
+]
+
+sreachArr2.forEach(d => {
+    d.ele.addEventListener('click', function () {
+        var inputValue = document.querySelector('input').value;
+        d.ele.href = d.url + inputValue;
+    })
+});
+
+allpic.addEventListener('click',function (){
+    var inputValue = document.querySelector('input').value;
+    sreachArr2.forEach(d => {
+        window.open(d.url + inputValue)
+    })
+}) 
+
+const sreachArr3 = [
+    {
+        ele: iconfont,
+        url: 'https://www.iconfont.cn/search/index?searchType=icon&q='
+    },
+    {
+        ele: unsplash,
+        url: 'https://unsplash.com/s/photos/'
+    },
+]
+
+sreachArr3.forEach(d => {
+    d.ele.addEventListener('click', function () {
+        var inputValue = document.querySelector('input').value;
+        d.ele.href = d.url + inputValue;
+    })
 })
 
-var google =document.querySelector('.button_google');
-google.addEventListener('click',function () {
+
+const sreachArr4 = [
+    {
+        url: 'https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&fm=index&pos=history&word='
+    },
+    {
+        url: 'https://www.google.com.hk/search?newwindow=1&safe=strict&hl=zh-CN&tbm=isch&sxsrf=ACYBGNSP1_41gP5pTvyBQnj6CR5Qd_Wfig%3A1574312459131&source=hp&biw=1240&bih=1329&ei=CxrWXZLeBZSu0PEP8a6FwAc&q='
+    },
+]
+
+image.addEventListener('click',function (){
     var inputValue = document.querySelector('input').value;
-    var addHref = 'https://www.google.com/search?sxsrf=ACYBGNRZUE-YO33VVcWqh2Z1CnoBtiY_UA%3A1574244452133&ei=ZBDVXZffB4K5wAPD9aOIAQ&q=' + inputValue;
-    google.href = addHref;
-})
-
-var bing = document.querySelector('.button_bing');
-bing.addEventListener('click',function (){
-    var inputValue = document.querySelector('input').value;
-    var addHref = 'https://www.bing.com/search?q=' + inputValue;
-    bing.href = addHref;   
-})
-
-var allweb =document.querySelector('.button_allweb');
-allweb.addEventListener('click', 
-
-    
-
-
-var dribbble = document.querySelector('.button_dribbble');
-dribbble.addEventListener('click', function () {
-    var inputValue = document.querySelector('input').value;
-    var addHref = 'https://dribbble.com/search/' + inputValue;
-    dribbble.href = addHref;
-})
-
-var behance = document.querySelector('.button_behance');
-behance.addEventListener('click', function () {
-    var inputValue = document.querySelector('input').value;
-    var addHref = 'https://www.behance.net/search?search=' + inputValue;
-    behance.href = addHref;
-})
-
-var pinterest = document.querySelector('.button_pinterest');
-pinterest.addEventListener('click', function () {
-    var inputValue = document.querySelector('input').value;
-    var addHref = 'https://www.pinterest.com/search/pins/?q=' + inputValue;
-    pinterest.href = addHref;
-})
+    sreachArr4.forEach(d => {
+        window.open(d.url + inputValue)
+    })
+}) 
 
 
 
@@ -52,18 +110,5 @@ pinterest.addEventListener('click', function () {
 
 
 
-var iconfont = document.querySelector('.button_iconfont');
-iconfont.addEventListener('click', function () {
-    var inputValue = document.querySelector('input').value;
-    var addHref = 'https://www.iconfont.cn/search/index?searchType=icon&q=' + inputValue;
-    iconfont.href = addHref;
-})
-
-var unsplash = document.querySelector('.button_unsplash');
-unsplash.addEventListener('click', function () {
-    var inputValue = document.querySelector('input').value;
-    var addHref = 'https://unsplash.com/s/photos/' + inputValue;
-    unsplash.href = addHref;
-})
 
 
